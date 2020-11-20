2977d64c23acbd22fe72b0e78d4bfcd0772fa43e
Matt Mason <masonmj10@gmail.com>
2020-11-20 01:16:23 -0500

    commit tree 199639bd83c23da5d6268c0d87233c6e68a2ca72

--- nested.rb
+++ nested.rb
@@ -14,9 +14,10 @@ def hopper
           :known_for => "Unix",
           :languages => ["C"]
         }
+        
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,7 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+     programmer_hash.merge(:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]})
     
 end
 
@@ -101,7 +104,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
 end
 
@@ -121,5 +125,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+     programmer_hash
 end
